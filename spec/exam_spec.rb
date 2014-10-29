356a615fdd195a7170e86f67b8baeb7e726c128c
alu0100763379 <alu0100@ull.edu.es>
2014-10-29 13:17:23 +0000

    Pregunta y respuesta guardada

--- spec/exam_spec.rb
+++ spec/exam_spec.rb
@@ -9,9 +9,15 @@ describe Exam do
 	end
 
 	describe "# Asignado la pregunta a la variable" do
+		
   	    it "Pregunta guardada." do
-		  @sS.pregunta.should eq("Que seleccion es la ganadora del mundial de futbol de 2014?")
+			@sS.pregunta.should eq("Que seleccion es la ganadora del mundial de futbol de 2014?")
 	    end
+		it "Respuestas guardadas" do
+			@sS.respuesta.should eq(["Italia","Francia","Alemania","Argentina"])
+		end
+		it "Todo guardado" do
+		@sS.to_s.should eq("Que seleccion es la ganadora del mundial de futbol de 2014?\na) Italia\nb) Francia\nc) Alemania\nd) Argentina\n")
+		end
 	end
-
 end

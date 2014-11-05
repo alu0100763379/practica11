90d25af3e8ccc74175ac428b0428053a4fd9ae7a
oscartrujillo <oscartrujillo94@gmail.com>
2014-11-05 12:34:48 +0000

    Lista doblemente enlazada

--- lib/exam/ll.rb
+++ lib/exam/ll.rb
@@ -1,23 +1,43 @@
 #Clase para representar la Lista
 
-Nodo = Struct.new(:value, :next_node)
+Nodo = Struct.new(:value, :next_node, :prev_node)
 
 class Lista
 
-    attr_accessor :head, :value, :next_node
+    attr_accessor :head, :value, :next_node, :tail
 
     def initialize
         @head = nil
+        @tail = nil
+    end
+    
+    def add_0(nodo)
+        nodo.next_node = @head
+        nodo.prev_node = @tail
+        @head = nodo
+        @tail = nodo
+        
     end
 
     def add(nodo)
-       
         nodo.next_node = @head
+        @head.prev_node = nodo
         @head = nodo
+        
     end
 
     def remove
         @head = @head.next_node
+    end
+    
+    def remove_d(nodo)
+        @tail = @tail.prev_node
+    end
+    
+     def add_d(nodo)
+        nodo.prev_node = @tail
+        @tail.next_node = nodo
+        @tail = nodo
     end
     
 end
